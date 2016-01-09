610038514f4aefe76d79c215a99537f4ae5d6209
Dayne Arron <design@daynearron.com>
2016-01-08 20:35:08 -0800

    Added Content to Welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
+  	@posts = Post.all.order("created_at desc")
+  	@insurances = Insurance.all.order("created_at desc")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
   resources :insurances
+  get 'welcome/index'
   root 'welcome#index'
 end
