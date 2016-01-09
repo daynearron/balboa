9b638683d54afa7bf8872ef582e1c5bf66b08225
Dayne Arron <design@daynearron.com>
2016-01-08 17:50:17 -0800

    Added Post Functionality and Welcome Page

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+  	@posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+			redirect_to @post, notice: "Success"
+  	else
+  		render 'new', notice: "Failed"
+  	end	
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+	  if @post.update(post_params)
+	  	redirect_to @post, notice: "Your article was successfully saved."
+	  else
+	 		render 'edit'
+	 	end
+  end
+
+  def destroy
+  	@post.destroy
+  	redirect_to action: "index"
+  end
+
+  private
+
+  def find_post
+  	@post = Post.find(params[:id])
+  end
+
+  def post_params
+  	params.require(:post).permit(:title, :content)
+  end
+end
