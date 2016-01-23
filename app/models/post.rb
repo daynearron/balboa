5d212a9ca169a92f39f46c16ed820e2fd8ac8af3
Dayne Arron <design@daynearron.com>
2016-01-22 17:16:15 -0800

    Added Path to Paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
 	extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+  has_attached_file :image, 
+  									:styles => { large: "600x600>", medium: "300x300>", thumb: "150x150#"},
+  									:storage => :s3,
+  									:path => "/images/:id/:style.:extension",
+  									:url => ":s3_domain_url"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
