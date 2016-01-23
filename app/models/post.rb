cffa490f9aa48620e459940d8d9cc6ebdc8a37a5
Dayne Arron <design@daynearron.com>
2016-01-22 17:32:08 -0800

    added url

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}, :url =>':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename'
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}, :url =>'s3-us-west-1.amazonaws.com', :path => '/:class/:attachment/:id_partition/:style/:filename'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
