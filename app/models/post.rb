4ca93bc3a86e007d343535e8e584d215edc7f608
Dayne Arron <design@daynearron.com>
2016-01-22 17:53:04 -0800

    Added host name

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,6 @@ class Post < ActiveRecord::Base
 	extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"},
-                  storage: :s3,
-                  s3_credentials: {access_key_id: ENV["AWS_KEY"], secret_access_key: ENV["AWS_SECRET"]},
-                  bucket: "balboapacific"
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,10 @@ Rails.application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => 'us-west-1',
+      :s3_host_name => 's3-us-west-1.amazonaws.com',
+      :url => ":s3_domain_url"
     }
   }
 end
