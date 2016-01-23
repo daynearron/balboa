62e75efe76004bd6e372cd9f560b01c4d83fa386
Dayne Arron <design@daynearron.com>
2016-01-22 17:41:03 -0800

    added env

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
 	extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}, :url =>'s3-us-west-1.amazonaws.com', :path => '/:class/:attachment/:id_partition/:style/:filename'
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"},
+                  storage: :s3,
+                  s3_credentials: {access_key_id: ENV["AWS_KEY"], secret_access_key: ENV["AWS_SECRET"]},
+                  bucket: "balboapacific"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
