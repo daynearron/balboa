807d79d32236917b3226380cc7c5879a9a67e046
Dayne Arron <design@daynearron.com>
2016-03-10 20:02:54 -0800

    more styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-  	@posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page:4)
+  	@posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page:5)
   	@insurances = Insurance.all.order("created_at desc").paginate(page: params[:page], per_page: 4)
   end
 
